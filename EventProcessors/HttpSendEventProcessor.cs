7b7726dc152b0c7664b9b7c28363314f2dcffdb8
Jason Young <jason@ytechie.com>
2014-10-21 16:03:09 -0500

    Multiple http sender improvements

--- EventProcessors/HttpSendEventProcessor.cs
+++ EventProcessors/HttpSendEventProcessor.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
 using System.Net;
 using System.Net.Http;
 using System.Net.Http.Formatting;
@@ -9,7 +11,6 @@ using Manufacturing.Framework.Datasource;
 using Manufacturing.Framework.Dto;
 using Microsoft.Practices.ServiceLocation;
 using Microsoft.ServiceBus.Messaging;
-using WorkerRoleWithSBQueue1.Configuration;
 
 namespace Manufacturing.FacilityDataProcessor.EventProcessors
 {
@@ -49,12 +50,26 @@ namespace Manufacturing.FacilityDataProcessor.EventProcessors
                 records.AddRange(newRecords);
             }
 
+#if(DEBUG)
+            const string postUrl = "http://localhost:3184/api/data";
+#else
+            const string postUrl = "http://mfxapi.azurewebsites.net/api/data";
+#endif
             var content = new ObjectContent<List<DatasourceRecord>>(records, new JsonMediaTypeFormatter());
 
-            var postResult = await client.PostAsync("http://localhost:3184/api/data", content);
-            if (postResult.StatusCode == HttpStatusCode.Created)
+            var sw = new Stopwatch();
+            sw.Start();
+            var postResult = await client.PostAsync(postUrl, content);
+            sw.Stop();
+            Log.DebugFormat("Posted {0} messages to '{1}' in {2}ms", records.Count, postUrl, sw.ElapsedMilliseconds);
+
+            if (postResult.StatusCode == HttpStatusCode.Created || postResult.StatusCode == HttpStatusCode.NoContent)
             {
                 await context.CheckpointAsync();
+            }
+            else
+            {
+                Log.WarnFormat("Received status code '{0}' in HTTP POST", postResult.StatusCode);
             }
         }
 
