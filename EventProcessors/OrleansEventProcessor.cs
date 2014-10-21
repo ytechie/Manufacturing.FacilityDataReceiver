dd26d57066d4b334b0ea853d9637bdaa1e2f0c4c
Jason Young <jason@ytechie.com>
2014-10-21 16:02:19 -0500

    Disabled the orleans event sender

--- EventProcessors/OrleansEventProcessor.cs
+++ EventProcessors/OrleansEventProcessor.cs
@@ -1,112 +1,112 @@
-﻿using System.Collections.Generic;
-using System.Diagnostics;
-using System.IO;
-using System.Linq;
-using System.Reflection;
-using System.Threading.Tasks;
-using log4net;
-using Manufacturing.Framework.Datasource;
-using Manufacturing.Framework.Dto;
-using Microsoft.Practices.ServiceLocation;
-using Microsoft.ServiceBus.Messaging;
-using Newtonsoft.Json;
-using RestSharp;
-using RestSharp.Serializers;
-using WorkerRoleWithSBQueue1.Configuration;
+﻿//using System.Collections.Generic;
+//using System.Diagnostics;
+//using System.IO;
+//using System.Linq;
+//using System.Reflection;
+//using System.Threading.Tasks;
+//using log4net;
+//using Manufacturing.Framework.Datasource;
+//using Manufacturing.Framework.Dto;
+//using Microsoft.Practices.ServiceLocation;
+//using Microsoft.ServiceBus.Messaging;
+//using Newtonsoft.Json;
+//using RestSharp;
+//using RestSharp.Serializers;
+//using WorkerRoleWithSBQueue1.Configuration;
 
-namespace Manufacturing.FacilityDataProcessor.EventProcessors
-{
-    public class OrleansEventProcessor : IConsumerGroupEventProcessor
-    {
-        private static readonly ILog Log = LogManager.GetLogger(MethodBase.GetCurrentMethod().DeclaringType);
+//namespace Manufacturing.FacilityDataProcessor.EventProcessors
+//{
+//    public class OrleansEventProcessor : IConsumerGroupEventProcessor
+//    {
+//        private static readonly ILog Log = LogManager.GetLogger(MethodBase.GetCurrentMethod().DeclaringType);
 
-        private readonly CloudConfiguration _cloudConfig;
-        private readonly IDatasourceRecordSerializer _datasourceRecordSerializer;
+//        private readonly CloudConfiguration _cloudConfig;
+//        private readonly IDatasourceRecordSerializer _datasourceRecordSerializer;
 
-        private bool _shuttingDown;
+//        private bool _shuttingDown;
 
-        private const int MaxBatchSize = 5000;
-        private int MaxBatchTimeMS = 10000;
+//        private const int MaxBatchSize = 5000;
+//        private int MaxBatchTimeMS = 10000;
 
-        public OrleansEventProcessor()
-        {
-            //We have to get the config from the container unfortunately since this class
-            //is being constructed by the event processor host
-            _cloudConfig = ServiceLocator.Current.GetInstance<CloudConfiguration>();
-            _datasourceRecordSerializer = ServiceLocator.Current.GetInstance<IDatasourceRecordSerializer>();
-        }
+//        public OrleansEventProcessor()
+//        {
+//            //We have to get the config from the container unfortunately since this class
+//            //is being constructed by the event processor host
+//            _cloudConfig = ServiceLocator.Current.GetInstance<CloudConfiguration>();
+//            _datasourceRecordSerializer = ServiceLocator.Current.GetInstance<IDatasourceRecordSerializer>();
+//        }
 
-        public string ConsumerGroupName
-        {
-            get { return "OrleansEventProcessor"; }
-        }
+//        public string ConsumerGroupName
+//        {
+//            get { return "OrleansEventProcessor"; }
+//        }
 
-        public Task OpenAsync(PartitionContext context)
-        {
-            return Task.FromResult<object>(null);
-        }
+//        public Task OpenAsync(PartitionContext context)
+//        {
+//            return Task.FromResult<object>(null);
+//        }
 
-        public async Task ProcessEventsAsync(PartitionContext context, IEnumerable<EventData> messages)
-        {
-            var sw = new Stopwatch();
-            var batch = new List<EventData>();
+//        public async Task ProcessEventsAsync(PartitionContext context, IEnumerable<EventData> messages)
+//        {
+//            var sw = new Stopwatch();
+//            var batch = new List<EventData>();
 
-            sw.Start();
-            foreach (var message in messages)
-            {
-                batch.Add(message);
-                if (_shuttingDown || batch.Count >= MaxBatchSize || sw.ElapsedMilliseconds >= MaxBatchTimeMS)
-                {
-                    ProcessBatch(batch);
-                    await context.CheckpointAsync();
+//            sw.Start();
+//            foreach (var message in messages)
+//            {
+//                batch.Add(message);
+//                if (_shuttingDown || batch.Count >= MaxBatchSize || sw.ElapsedMilliseconds >= MaxBatchTimeMS)
+//                {
+//                    ProcessBatch(batch);
+//                    await context.CheckpointAsync();
 
-                    batch.Clear();
-                    sw.Reset();
-                    sw.Start();
-                }
+//                    batch.Clear();
+//                    sw.Reset();
+//                    sw.Start();
+//                }
 
-                if (_shuttingDown)
-                {
-                    return;
-                }
-            }
+//                if (_shuttingDown)
+//                {
+//                    return;
+//                }
+//            }
 
-            if (batch.Count > 0)
-            {
-                ProcessBatch(batch);
-                await context.CheckpointAsync();
-            }
-        }
+//            if (batch.Count > 0)
+//            {
+//                ProcessBatch(batch);
+//                await context.CheckpointAsync();
+//            }
+//        }
 
-        public Task CloseAsync(PartitionContext context, CloseReason reason)
-        {
-            _shuttingDown = true;
-            return null;
-        }
+//        public Task CloseAsync(PartitionContext context, CloseReason reason)
+//        {
+//            _shuttingDown = true;
+//            return null;
+//        }
 
-        private void ProcessBatch(IList<EventData> messages)
-        {
-            var records = new List<DatasourceRecord>();
+//        private void ProcessBatch(IList<EventData> messages)
+//        {
+//            var records = new List<DatasourceRecord>();
 
-            foreach (var message in messages)
-            {
-                var body = message.GetBody<Stream>();
-                var deserializedRecords = _datasourceRecordSerializer.Deserialize(body);
-                records.AddRange(deserializedRecords);
-            }
+//            foreach (var message in messages)
+//            {
+//                var body = message.GetBody<Stream>();
+//                var deserializedRecords = _datasourceRecordSerializer.Deserialize(body);
+//                records.AddRange(deserializedRecords);
+//            }
 
-            InsertRecords(records);
-        }
+//            InsertRecords(records);
+//        }
 
-        private void InsertRecords(IEnumerable<DatasourceRecord> recordBatch)
-        {
-            var client = new RestClient(_cloudConfig.OrleansUrl);
+//        private void InsertRecords(IEnumerable<DatasourceRecord> recordBatch)
+//        {
+//            var client = new RestClient(_cloudConfig.OrleansUrl);
 
-            var json = JsonConvert.SerializeObject(recordBatch);
-            var request = new RestRequest("api/DataRecord", Method.POST);
-            request.AddParameter("application/json", json, ParameterType.RequestBody);
+//            var json = JsonConvert.SerializeObject(recordBatch);
+//            var request = new RestRequest("api/DataRecord", Method.POST);
+//            request.AddParameter("application/json", json, ParameterType.RequestBody);
 
-            var res = client.Execute(request);
-        }
-    }
-}
+//            var res = client.Execute(request);
+//        }
+//    }
+//}
